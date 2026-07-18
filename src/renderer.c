b9ab91ab1fcab4ba6ab228b5ad3ac1201de7acca
Alex <alex.ponomar37@gmail.com>
2026-07-18 03:42:34 -0500

    renderer.c prototype

--- src/renderer.c
+++ src/renderer.c
@@ -16,7 +16,6 @@ renderer_length
 
 /* Function Definitions*/
 
-
 int renderer_init(Renderer* renderer, int term_w, int term_h) {
     if (!renderer || term_h<1 || term_w<1) return -1;
 
@@ -33,26 +32,56 @@ int renderer_init(Renderer* renderer, int term_w, int term_h) {
     return 0;
 }
 
-
-void draw_frame(WeatherCondition Weather, int frame) {
-    printf("cweather\t");
-    printf("Frame: %d\n", frame);
-    printf("Condition: rain\n");
-    
-    for ( int i = 0; i < SCENE_HEIGHT; i++ ) {
-        for ( int j = 0; j < SCENE_WIDTH; j++ ) {
-            if ( (i * 3 + j + frame) % 17 == 0 ) {
-                putchar('/');
-            }
-            else putchar(' ');
-        }
-        putchar('\n'); // adds a row
-    }
-    for ( int j = 0; j < SCENE_WIDTH; j++ ) {
-        // print a simple ground
-        putchar('^');
-    }
-
-    putchar('\n');
-    printf("Made by russkiyximik");
+void renderer_destroy(Renderer* renderer) {
+    free(renderer); // ?
 }
+
+
+void renderer_clear(Renderer* renderer) {
+    for ( int row = 0; row < renderer->height; row++ ) {
+        // offset is row * renderer->stride
+        // leave spaces for \n
+        memset(renderer->buffer + renderer->stride * row, 
+            0, renderer->width); // should i set it to blank space ' ' instead?
+    }
+}
+
+int renderer_write(Renderer* renderer, int x, int y, char symbol) {
+    // ensure (x, y) within bounds
+    if (y > renderer->height || x > renderer->width) return -1;
+
+    memset(renderer->buffer + renderer->stride * y + x, symbol, 1);
+    return 0;
+}
+
+
+const char* renderer_buffer(const Renderer* renderer) {return renderer->buffer;}
+const size_t renderer_length(const Renderer* renderer) {return renderer->length;}
+
+
+
+
+
+// Random stuff (tbd whether needed or not)
+// void draw_frame(WeatherCondition Weather, int frame) {
+//     printf("cweather\t");
+//     printf("Frame: %d\n", frame);
+//     printf("Condition: rain\n");
+    
+//     for ( int i = 0; i < SCENE_HEIGHT; i++ ) {
+//         for ( int j = 0; j < SCENE_WIDTH; j++ ) {
+//             if ( (i * 3 + j + frame) % 17 == 0 ) {
+//                 putchar('/');
+//             }
+//             else putchar(' ');
+//         }
+//         putchar('\n'); // adds a row
+//     }
+//     for ( int j = 0; j < SCENE_WIDTH; j++ ) {
+//         // print a simple ground
+//         putchar('^');
+//     }
+
+//     putchar('\n');
+//     printf("Made by russkiyximik");
+// }
